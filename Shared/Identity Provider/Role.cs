91327a6d84b547ae2321d8a5d56d8c9e06e27b58
Adrian <sci.adrian@gmail.com>
2018-11-05 22:08:29 +0200

    SHARED-1 Shared project created

---
+++ Shared/Identity Provider/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Identity_Provider
+{
+    public enum Role
+    {
+        Manager = 1,
+        Help = 2,
+        Student = 3,
+        Candidate = 4
+    }
+}
---
+++ Shared/Identity Provider/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Identity_Provider
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public Role role { get; set; }
+    }
+}
