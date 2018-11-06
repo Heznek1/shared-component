3fb9959050999e584fe6a75017e5157a6496f31e
Adrian <sci.adrian@gmail.com>
2018-11-06 09:54:56 +0200

    Added more shared components

---
+++ Shared/Encryption/EncriptPassword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Shared.Encryption
+{
+    public static class EncriptPassword
+    {
+        public static string EncryptPassword(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentNullException();
+            }
+
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+            var hashBytes = new SHA512Managed().ComputeHash(inputBytes);
+            var hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+
+            return hash;
+        }
+    }
+}
---
+++ Shared/Security/Token.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace Shared.Security
+{
+    public static class Token
+    {
+        private static readonly string secretKey = "Secret";
+
+        public static string GetSecretKey()
+        {
+            return ConfigurationManager.AppSettings[secretKey];
+        }
+    }
+}
