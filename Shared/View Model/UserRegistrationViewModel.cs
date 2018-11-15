90180e283598c3c98e1bef4675da6b410a61542b
Adrian <sci.adrian@gmail.com>
2018-11-15 17:04:07 +0200

    Added user registration model

---
+++ Shared/View Model/UserRegistrationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Shared.View_Model
+{
+    public class UserRegistrationViewModel
+    {
+        public UserRegistrationViewModel()
+        {
+            guid = Guid.NewGuid().ToString();
+        }
+
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string confirm { get; set; }
+        public string guid { get; }
+    }
+}
