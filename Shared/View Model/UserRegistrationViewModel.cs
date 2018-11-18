4295352b94724accc5c0e1ccfcb24ce5b7a6a93a
Adrian <sci.adrian@gmail.com>
2018-11-18 14:57:48 +0200

    Updated shared component

--- Shared/View Model/UserRegistrationViewModel.cs
+++ Shared/View Model/UserRegistrationViewModel.cs
@@ -7,12 +7,17 @@ namespace Shared.View_Model
         public UserRegistrationViewModel()
         {
             guid = Guid.NewGuid().ToString();
+            instance = "heznekservice";
         }
 
         public string username { get; set; }
         public string email { get; set; }
         public string password { get; set; }
         public string confirm { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string phone { get; set; }
         public string guid { get; }
+        public string instance { get; }
     }
 }
